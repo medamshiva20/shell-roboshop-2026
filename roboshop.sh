4f0c93268416e62e461e6d30f0d90afe98136315
medamshiva20 <medamshiva20@gmail.com>
2026-02-01 14:17:40 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,10 @@ for instance in $@
 do
   INSTANCE_ID=$( aws ec2 run-instances \
     --image-id ami-0220d79f3f480ecf5 \
-    --instance-type t2.micro \
+    --instance-type "t2.micro" \
     --subnet-id subnet-0e2d2a34508988aa6 \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance},{Key=Environment,Value=Test}]' 'ResourceType=volume,Tags=[{Key=Project,Value=Marketing}]' )
+    --security-group-ids sg-0e952a919361d18ab \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
 done
