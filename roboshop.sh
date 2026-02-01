b0151ac82819673d1f003ad8457c85bc865e1b77
medamshiva20 <medamshiva20@gmail.com>
2026-02-01 13:56:26 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -7,9 +7,9 @@ SUBNET_ID="subnet-0e2d2a34508988aa6"
 
 for instance in $@
 do
-  INSTANCE_ID=$(aws ec2 run-instances \
+  INSTANCE_ID=$( aws ec2 run-instances \
     --image-id ami-0220d79f3f480ecf5 \
     --instance-type t2.micro \
     --subnet-id subnet-0e2d2a34508988aa6 \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance},{Key=Environment,Value=Test}]' 'ResourceType=volume,Tags=[{Key=Project,Value=Marketing}]')
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance},{Key=Environment,Value=Test}]' 'ResourceType=volume,Tags=[{Key=Project,Value=Marketing}]' )
 done
