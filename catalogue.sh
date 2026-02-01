99a0d56eb683ef712001d4f90e852b81a3752227
medamshiva20 <medamshiva20@gmail.com>
2026-02-01 19:29:29 +0530

    creating catalogue configuration

--- catalogue.sh
+++ catalogue.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 USERID=$(id -u)
 LOGS_DIR="/var/log/shell-roboshop"
@@ -7,19 +7,22 @@ LOG_FILE="$LOGS_DIR/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ] ;
 then 
-    echo -e "$R Please run this script with root user $N"
+    echo -e "$R Please run this script with root user $N" | tee -a $LOG_FILE
     exit 1
 fi
 
+mkdir -p $LOGS_DIR
+
 VALIDATE(){
     if [ $1 -ne 0 ] ;
-    then
-        echo -e "$2...$R FAILURE $N"
+    then 
+        echo -e "$2...$R FAILURE $N" | tee -a $LOG_FILE
     else
-        echo -e "$2...$G SUCCESS $N"
+        echo -e "$2...$G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
