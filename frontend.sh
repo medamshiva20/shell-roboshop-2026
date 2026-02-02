9205b9ca813b85f6cfb331b11a3c518e1cd7663a
medamshiva20 <medamshiva20@gmail.com>
2026-02-02 23:54:30 +0530

    frontend configuration

--- frontend.sh
+++ frontend.sh
@@ -10,13 +10,23 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+mkdir -p $LOGS_DIR
+
 if [ $USERID -ne 0 ] ;
 then 
     echo -e "$R Please run this script with root user $N" | tee -a $LOG_FILE
     exit 1
 fi 
 
-
+VALIDATE(){
+    if [ $1 -ne 0 ] ;
+    then 
+        echo -e "$2 ...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ...$G SUCCESS $N"
+    fi
+}
 
 
 dnf module disable nginx -y &>>$LOG_FILE
