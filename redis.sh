8b5b7c22c7fb49e2b03a6aec771500113b8b920a
medamshiva20 <medamshiva20@gmail.com>
2026-02-02 20:37:30 +0530

    redis configuration

--- redis.sh
+++ redis.sh
@@ -15,7 +15,15 @@ then
     exit 1
 fi 
 
-
+VALIDATE(){
+    if [ $1 -ne 0 ] ;
+    then 
+        echo -e "$2 ...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ...$G SUCCESS $N"
+    fi
+}
 
 dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling default redis version"
